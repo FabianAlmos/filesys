9e51cf25374c708942eb8c8cc6ffca0ec61b8118
Fábián Álmos <fabian.almos77@gmail.com>
2023-05-19 22:34:46 +0200

    Renamed userRepo.go to user.go

--- db/repo/user.go
+++ db/repo/user.go
@@ -1,1 +1,11 @@
 package repo
+
+import "filesys/db/model"
+
+type UserRepositoryI interface {
+	Create(user *model.User) (int32, error)
+	GetByEmail(email *string) (*model.User, error)
+	GetAll() (*[]model.User, error)
+	Update(user *model.User) (*model.User, error)
+	Delete(id int32) error
+}
