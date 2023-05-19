c686f91600df4f87ddf62e240f2ec21d312c3012
Fábián Álmos <fabian.almos77@gmail.com>
2023-05-19 22:22:14 +0200

    Created User struct

---
+++ db/model/User.go
@@ -0,0 +1,11 @@
+package model
+
+type User struct {
+	ID        int    `json:"id"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	CreatedAt string `json:"created_at"`
+	UpdateAt  string `json:"updated_at"`
+}
