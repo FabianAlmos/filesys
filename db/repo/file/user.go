81d400ec783e26b181c986c5655237eccd1f50a2
Fábián Álmos <fabian.almos77@gmail.com>
2023-05-19 23:46:43 +0200

    Added UserRepo functions

--- db/repo/file/user.go
+++ db/repo/file/user.go
@@ -1,4 +1,44 @@
 package file
 
-type UserRepository struct {
+import (
+	"bufio"
+	"encoding/json"
+	"filesys/db/model"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type UserRepository struct{}
+
+func (ur *UserRepository) Create(user *model.User) (int32, error) {
+	file, err := os.Create(strconv.Itoa(int(user.ID)))
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	writer := bufio.NewWriter(file)
+	json, err := json.Marshal(user)
+	if err != nil {
+		fmt.Println(err)
+	}
+	writer.WriteString(string(json))
+	writer.Flush()
+	return user.ID, err
 }
+
+/*func (ur *UserRepository) GetByEmail(email *string) (*model.User, error) {
+
+}
+
+func (ur *UserRepository) GetAll() (*[]model.User, error) {
+
+}
+
+func (ur *UserRepository) Update() (*model.User, error) {
+
+}
+
+func (ur *UserRepository) Delete() error {
+
+}*/
